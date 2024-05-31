1f86a488b890a0aaf22cafc8102b7d02bdf008bb
Noam Chaize--Assba <nchaize-@z3r2p3.42lyon.fr>
2024-05-31 14:08:56 +0200

    freed better in parsing

--- src/parsing/parse_line.c
+++ src/parsing/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: nchaize- <@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 09:47:29 by nchaize-          #+#    #+#             */
-/*   Updated: 2024/05/31 12:52:01 by nchaize-         ###   ########.fr       */
+/*   Updated: 2024/05/31 14:08:15 by nchaize-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ t_command	*parse_line(char *line, t_data *data)
 	i = 0;
 	j = 0;
 	if (!checker(&line, data))
-		return (data->last_error = 2, NULL);
+		return (free(line), data->last_error = 2, NULL);
 	args = count_args(line);
 	parsed = malloc(sizeof(t_command) * (args + 1));
 	if (!parsed)
@@ -103,6 +103,6 @@ t_command	*parse_line(char *line, t_data *data)
 	}
 	parsed[j].command = NULL;
 	if (!last_check(parsed))
-		return (free(line), NULL);
+		return (free(line), free_parsing(parsed, j), NULL);
 	return ((free(line), finish_parsing(parsed)));
 }
